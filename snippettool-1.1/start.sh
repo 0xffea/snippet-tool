e311768f8fd578953665b32e0b63ada463a4f9ea
silvestre@zabala.name <silvestre@zabala.name@ca1235d4-b34b-0410-9abf-e3faba0e8c72>
2009-09-30 13:35:59 +0000

    Propagate command line.
    git-svn-id: http://snippet-tool.googlecode.com/svn/trunk@162 ca1235d4-b34b-0410-9abf-e3faba0e8c72

--- snippettool-1.1/start.sh
+++ snippettool-1.1/start.sh
@@ -1,1 +1,1 @@
-java -Xms512m -Xmx512m -XX:PermSize=256m -XX:MaxPermSize=256m -Dfile.encoding=UTF-8 -jar snippettool-1.1.jar
+java -Xms512m -Xmx512m -XX:PermSize=256m -XX:MaxPermSize=256m -Dfile.encoding=UTF-8 -jar snippettool-1.1.jar $@
