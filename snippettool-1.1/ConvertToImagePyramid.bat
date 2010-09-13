d54d7d7563dcfb8cc6e104eaa64269eca43ee962
silvestre@zabala.name <silvestre@zabala.name@ca1235d4-b34b-0410-9abf-e3faba0e8c72>
2010-09-13 22:33:17 +0000

    Change conversion to deflate.
    git-svn-id: http://snippet-tool.googlecode.com/svn/trunk@182 ca1235d4-b34b-0410-9abf-e3faba0e8c72

--- snippettool-1.1/ConvertToImagePyramid.bat
+++ snippettool-1.1/ConvertToImagePyramid.bat
@@ -5,4 +5,4 @@ set infile=%1
 set outfile=%infile:~0,-4%
 
 @echo on
-%vipsbin%\vips im_vips2tiff %infile% %outfile%.tif:jpeg:90,tile:256x256,pyramid
+%vipsbin%\vips im_vips2tiff %infile% %outfile%.tif:deflate,tile:256x256,pyramid
